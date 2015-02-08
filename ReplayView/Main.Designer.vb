495cd3ecdbc6a8ae584a4186464e4b4d00ead3aa
ExcelledProducts <papandreavittorio@gmail.com>
2015-02-08 13:12:43 -0800

    Added init commit.

---
+++ ReplayView/Main.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnStartReplay = New System.Windows.Forms.Button()
+        Me.btnStartConversion = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.lbProgress = New System.Windows.Forms.Label()
+        Me.bgwConvert = New System.ComponentModel.BackgroundWorker()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbReady = New System.Windows.Forms.Label()
+        Me.chRightJoystick = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chLeftJoystick = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chLeftMotors = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chRightMotors = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chRightEncoder = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chLeftEncoder = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chGyro = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chRightEncoderChange = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chLeftEncoderChange = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chGyroChange = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnStartReplay
+        '
+        Me.btnStartReplay.Enabled = False
+        Me.btnStartReplay.Location = New System.Drawing.Point(617, 280)
+        Me.btnStartReplay.Name = "btnStartReplay"
+        Me.btnStartReplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnStartReplay.TabIndex = 2
+        Me.btnStartReplay.Text = "Start Replay"
+        Me.btnStartReplay.UseVisualStyleBackColor = True
+        '
+        'btnStartConversion
+        '
+        Me.btnStartConversion.Location = New System.Drawing.Point(1146, 12)
+        Me.btnStartConversion.Name = "btnStartConversion"
+        Me.btnStartConversion.Size = New System.Drawing.Size(99, 23)
+        Me.btnStartConversion.TabIndex = 3
+        Me.btnStartConversion.Text = "Start Conversion"
+        Me.btnStartConversion.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 251)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(1191, 23)
+        Me.ProgressBar1.TabIndex = 12
+        '
+        'lbProgress
+        '
+        Me.lbProgress.AutoSize = True
+        Me.lbProgress.Location = New System.Drawing.Point(1212, 255)
+        Me.lbProgress.Name = "lbProgress"
+        Me.lbProgress.Size = New System.Drawing.Size(33, 13)
+        Me.lbProgress.TabIndex = 13
+        Me.lbProgress.Text = "100%"
+        '
+        'bgwConvert
+        '
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chRightJoystick, Me.chLeftJoystick, Me.chLeftMotors, Me.chRightMotors, Me.chRightEncoder, Me.chLeftEncoder, Me.chGyro, Me.chRightEncoderChange, Me.chLeftEncoderChange, Me.chGyroChange})
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(15, 41)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(1230, 204)
+        Me.ListView1.TabIndex = 14
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Status:"
+        '
+        'lbReady
+        '
+        Me.lbReady.AutoSize = True
+        Me.lbReady.ForeColor = System.Drawing.Color.Green
+        Me.lbReady.Location = New System.Drawing.Point(58, 17)
+        Me.lbReady.Name = "lbReady"
+        Me.lbReady.Size = New System.Drawing.Size(94, 13)
+        Me.lbReady.TabIndex = 16
+        Me.lbReady.Text = "Ready To Convert"
+        '
+        'chRightJoystick
+        '
+        Me.chRightJoystick.Text = "Right Joystick"
+        Me.chRightJoystick.Width = 86
+        '
+        'chLeftJoystick
+        '
+        Me.chLeftJoystick.Text = "Left Joystick"
+        Me.chLeftJoystick.Width = 79
+        '
+        'chLeftMotors
+        '
+        Me.chLeftMotors.Text = "Left Motors Voltage"
+        Me.chLeftMotors.Width = 111
+        '
+        'chRightMotors
+        '
+        Me.chRightMotors.Text = "Right Motors Voltage"
+        Me.chRightMotors.Width = 121
+        '
+        'chRightEncoder
+        '
+        Me.chRightEncoder.Text = "Right Encoder"
+        Me.chRightEncoder.Width = 99
+        '
+        'chLeftEncoder
+        '
+        Me.chLeftEncoder.Text = "Left Encoder"
+        Me.chLeftEncoder.Width = 97
+        '
+        'chGyro
+        '
+        Me.chGyro.Text = "Gryo Angle"
+        Me.chGyro.Width = 100
+        '
+        'chRightEncoderChange
+        '
+        Me.chRightEncoderChange.Text = "Right Encoder Change"
+        Me.chRightEncoderChange.Width = 127
+        '
+        'chLeftEncoderChange
+        '
+        Me.chLeftEncoderChange.Text = "Left Encoder Change"
+        Me.chLeftEncoderChange.Width = 117
+        '
+        'chGyroChange
+        '
+        Me.chGyroChange.Text = "Gyro Change"
+        Me.chGyroChange.Width = 79
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1146, 280)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Toggle Display"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1253, 314)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lbReady)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.lbProgress)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnStartConversion)
+        Me.Controls.Add(Me.btnStartReplay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmMain"
+        Me.Text = "Replay Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnStartReplay As System.Windows.Forms.Button
+    Friend WithEvents btnStartConversion As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents lbProgress As System.Windows.Forms.Label
+    Friend WithEvents bgwConvert As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents chRightJoystick As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chLeftJoystick As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chLeftMotors As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chRightMotors As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chRightEncoder As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chLeftEncoder As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chGyro As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lbReady As System.Windows.Forms.Label
+    Friend WithEvents chRightEncoderChange As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chLeftEncoderChange As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chGyroChange As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ ReplayView/Main.vb
@@ -0,0 +1,89 @@
+﻿Public Class frmMain
+
+    Dim replayFilePath As String
+    Dim replayFile() As String
+
+    Dim prevEncoderRight As Double = 0.0
+    Dim prevEncoderLeft As Double = 0.0
+    Dim prevGyroAngle As Double = 0.0
+
+    Dim Display As Boolean = True
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Control.CheckForIllegalCrossThreadCalls = False
+
+        Dim filePicker As New OpenFileDialog
+        filePicker.ShowDialog()
+        replayFilePath = filePicker.FileName
+
+        replayFile = System.IO.File.ReadAllLines(replayFilePath)
+
+        Dim timeElapsed As Double = ((replayFile.Count - 1) * (0.02)) / 60
+
+    End Sub
+
+    Private Sub btnStartConversion_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStartConversion.Click
+        btnStartConversion.Enabled = False
+        bgwConvert.RunWorkerAsync()
+    End Sub
+
+    Private Sub bgwConvert_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles bgwConvert.DoWork
+
+        Dim count As Integer = 0
+        Dim max As Integer = replayFile.Count
+
+        For Each line As String In replayFile
+
+            If Display Then
+                Dim displayArray() As String = {"", "", "", "", "", "", "", "", "", "", ""}
+                displayArray(0) = line.Split(":")(0).Split(",")(0)
+                displayArray(3) = "Unavalibal"
+
+                displayArray(1) = line.Split(":")(0).Split(",")(1)
+                displayArray(2) = "Unavalibal"
+
+                displayArray(4) = line.Split(":")(1).Split(",")(0)
+                displayArray(7) = CDbl(displayArray(4) - prevEncoderRight)
+                prevEncoderRight = CDbl(displayArray(4))
+
+                displayArray(5) = line.Split(":")(1).Split(",")(1)
+                displayArray(8) = CDbl(displayArray(1) - prevEncoderLeft)
+                prevEncoderRight = CDbl(displayArray(5))
+
+                displayArray(6) = line.Split(":")(2)
+                displayArray(9) = CDbl(displayArray(6) - prevGyroAngle)
+                prevGyroAngle = CDbl(displayArray(6))
+
+
+                Dim listItem As New ListViewItem(displayArray)
+                ListView1.Items.Add(listItem)
+
+                ListView1.EnsureVisible(ListView1.Items.Count - 1)
+                ListView1.Update()
+
+                'lbProgress.Text = CStr(Math.Round(count / max, 0))
+                'count += 1
+            End If
+            lbProgress.Text = CStr(Math.Round(count / max, 0) * 100)
+            ProgressBar1.Value = Math.Round(count / max, 0) * 100
+            count += 1
+        Next
+
+        MsgBox(count)
+        MsgBox(max)
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If (Display) Then
+            Display = False
+        Else
+            Display = True
+        End If
+    End Sub
+
+    Private Sub lbProgress_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbProgress.Click
+
+    End Sub
+End Class
