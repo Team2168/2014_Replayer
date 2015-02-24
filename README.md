7fdcb62be6f5cc560acd28b0804d171ce1901d04
ExcelledProducts <papandreavittorio@gmail.com>
2015-02-23 16:30:13 -0800

    remove data

--- README.md
+++
@@ -1,2 +0,0 @@
-# Replayer
-Replayer is a diagnositc application that replays an FRC match revealing all data and robot path durring the match. 
--- ReplayView/DisplayWindow.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class DisplayWindow
-
-    Dim f As Graphics = Me.CreateGraphics
-
-    Private Sub DisplayWindow_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
-
-End Class
